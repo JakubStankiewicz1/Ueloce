f3e3dc8f5ef566d6db6b85e0deac20ee7b7a0ec0
Kuba <272264@student.pwr.edu.pl>
2025-06-12 01:42:08 +0200

    Prepare to hosting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4011,
+  },
+  preview: {
+    port: 4011,
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
