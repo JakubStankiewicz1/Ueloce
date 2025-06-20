af0bcc9442c923c22156b02f098322d01d45ee42
Kuba <272264@student.pwr.edu.pl>
2025-06-20 20:56:48 +0200

    fixed arrow that isn`t displaying on the live page

--- frontend/src/components/HomeFindStocklist/HomeFindStocklist.jsx
+++ frontend/src/components/HomeFindStocklist/HomeFindStocklist.jsx
@@ -24,14 +24,12 @@ const HomeFindStocklist = () => {
                         <NavLink to='/find-us' className="homeFindStocklistContainerLeftContainerBottomContainer">
                             <p className="homeFindStocklistContainerLeftContainerBottomContainerTextOne jaro-regular">
                                 Find Stocklist
-                            </p>
-
-                            <p className="homeFindStocklistContainerLeftContainerBottomContainerTextTwo jaro-regular">
+                            </p>                            <p className="homeFindStocklistContainerLeftContainerBottomContainerTextTwo jaro-regular">
                                 -
                             </p>
 
                             <div className="homeFindStocklistContainerLeftContainerBottomContainerTextTwoThree jaro-regular">
-                                >
+                                →
                             </div>
                         </NavLink>
                     </div>
